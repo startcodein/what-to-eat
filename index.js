89e544176cb011f24c530232745333a378937f3a
Muhammed Athimannil <hello@athimannil.com>
2018-08-11 23:48:13 +0200

    add node

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(port, (err) => {
+  console.log('Server runs on localhost:3000')
+});
