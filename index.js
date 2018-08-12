b459dce4200393ba09d0cae09aa74d0fdd7d165b
Muhammed Athimannil <hello@athimannil.com>
2018-08-12 14:18:59 +0200

    more commants

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const bot = new Telegraf('671427209:AAHXiSMLorX1baVKssIAEm0kQjaM7mYcohw');
 
 // app.use(Telegraf.log())
 //
-// // Start command
-// app.command('start', ({ reply }) => {
-//   reply('Welcome, nice to meet you! I can sell you various products. Just ask.');
-// });
+// Start command
+app.command('start', ({ reply }) => {
+  reply('Welcome, nice to meet you! I can sell you various products. Just ask.');
+});
 
 bot.use((ctx) => {
   console.log(ctx.message)
   console.log('============================')
-  console.log(ctx.chat)
-  console.log('============================')
-  console.log(ctx.from)
-  console.log('============================')
-  console.log(ctx.match)
+  // console.log(ctx.chat)
+  // console.log('============================')
+  // console.log(ctx.from)
+  // console.log('============================')
+  // console.log(ctx.match)
 })
 
 bot.start((ctx) => ctx.reply('Welcome'))
@@ -25,5 +25,9 @@ bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy'))
 bot.hashtag('kooy', (ctx) => ctx.reply('Your number is'))
+
+bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('modern', ({ reply }) => reply('Yo'))
+bot.command('hipster', Telegraf.reply('λ'))
 
 bot.startPolling()
