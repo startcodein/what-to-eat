c04a975b5193a6b504d2d8fa627cc15491dfa7c5
Muhammed athimanil <hello@athimannil.com>
2018-09-01 01:52:50 +0200

    FIX: last_name shows onlky if available

--- actions/preventBot.js
+++ actions/preventBot.js
@@ -17,7 +17,7 @@ const preventBot = (ctx, next) => {
     ctx.telegram.kickChatMember(chatID, bot.id);
   }
 
-  ctx.replyWithMarkdown(`ഗ്രൂപ്പിൽ വന്ന ${bots.map(link).join(', ')} ബോട്ടിനെ പുറത്താക്കിയിരിക്കുന്നു. അതോടൊപ്പം ഈ ബോട്ടിനെ ഗ്രൂപ്പിൽ ചേർത്ത [${invitedUser.first_name} ${invitedUser.last_name}](tg://user?id=${invitedUser.id}) നെയും പുറത്താക്കിയിരിക്കുന്നു.`)
+  ctx.replyWithMarkdown(`ഗ്രൂപ്പിൽ വന്ന ${bots.map(link).join(', ')} ബോട്ടിനെ പുറത്താക്കിയിരിക്കുന്നു. അതോടൊപ്പം ഈ ബോട്ടിനെ ഗ്രൂപ്പിൽ ചേർത്ത [${invitedUser.first_name} ${invitedUser.last_name ||''}](tg://user?id=${invitedUser.id}) നെയും പുറത്താക്കിയിരിക്കുന്നു.`)
   return next();
 }
 
